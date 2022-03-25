0795e119c0208ae3b1670e92856c4780a5682f5b
Chris <e11776854@student.tuwien.ac.at>
2022-03-25 06:51:03 +0100

    Add project files.

---
+++ 1WA2/Car.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1WA2
+{
+    internal class Car
+    {
+        //properties
+        //Lesen Sie sich den Code durch und schreiben Sie sinnvolle Comments
+        public String CarDesc { get; set; }
+        public int FuelCap { get; set; }
+        private double fuel;
+        public double Fuel {
+            get
+            {
+                return this.fuel;
+            }
+            set
+            {
+                this.fuel = value;
+                
+                if (value >= this.FuelCap)
+                {
+                    this.fuel = this.FuelCap;
+                }
+                if (value < 0)
+                {
+                    this.fuel = 0;
+                }
+
+            }
+        }
+        public double FuelConsume { get; set; }
+        public double MaxRange { 
+            get
+            {
+                return this.fuel * 100 / this.FuelConsume;
+            }
+        }
+
+        public int DrivenKm { get; set; }
+
+
+        //constructor
+        public Car(String carDesc, int fuelCap, double fuel, double fuelConsume)
+        {
+            this.CarDesc = carDesc;
+            this.FuelCap = fuelCap;
+            this.fuel = fuel;
+            this.FuelConsume = fuelConsume;
+        }
+        //ToString Method
+        public override string ToString()
+        {
+            //hier werden alle Properties als string ausgeben
+            return "Model: " + CarDesc + 
+                ", Fuel Capacity: " + FuelCap +
+                ", Fuel Consumption: " + FuelConsume +
+                ", Fuel: " + fuel +
+                ", DrivenKm: " + DrivenKm;
+        }
+
+        public void Drive(int distance)
+        {
+            if (distance < 0)
+            {
+                return;
+            }
+            double usedFuel = this.FuelConsume * distance / 100;
+            if (usedFuel > this.fuel)
+            {
+                return;
+            }
+            this.Fuel -= usedFuel;
+            this.DrivenKm += distance;
+        }
+
+    }
+}
---
+++ 1WA2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _1WA2;
+
+Car Car1 = new Car("Audi", 60, 10.0, 0.7);
+Console.WriteLine(Car1);
+Car1.CarDesc = "BWM";
+Console.WriteLine(Car1.MaxRange);
+Car1.Fuel = 1000;
+Console.WriteLine(Car1.MaxRange);
+
+Car1.Drive(100);
+
+Console.WriteLine(Car1);
+
+Car1.Drive(10000);
+Console.WriteLine(Car1);
+
+Car1.Drive(100);
+Console.WriteLine(Car1);
+
+Car1.Drive(-10000);
+Console.WriteLine(Car1);
